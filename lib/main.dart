03036dc170c03746c5c57f6b64e1ae71d05c8159
LAGUNHO <100767288+gunho30811@users.noreply.github.com>
2023-03-07 10:58:58 +0900

    2023-03-07 10:58
    기본적 UI 삭제 후, lib에 model, scrren widget 폴더 추가하고 기본적인 앱 슬라이드 생성

--- lib/main.dart
+++ lib/main.dart
@@ -1,61 +1,36 @@
+import 'package:contact_youtube/widget/bottom_bar.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp()); //App start
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  _MyAppState createState() => _MyAppState();
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class _MyAppState extends State<MyApp> {
+  late TabController controller;
   @override
   Widget build(BuildContext context) {
-    // return const Placeholder(); 초기 설정으로 나옴. 혹시 몰라서 주석처리
-
-    return MaterialApp(  //앱 디자인은 위젯 짜집기
-      home: Scaffold(
-        appBar: AppBar(),
-        body: Container(
-          height: MediaQuery.of(context).size.height * 0.3,
-          width: MediaQuery.of(context).size.width*1,
-          padding: EdgeInsets.all(10),
-          child: Row(
-            children: [
-              Image.asset('car.jpg', width:MediaQuery.of(context).size.width*0.2,),
-              Container(
-                width: MediaQuery.of(context).size.width *0.7,
-                margin: EdgeInsets.fromLTRB(0, 20, 0, 0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text('캐논 DSLR 100D (단렌즈,충전기 16기가SD 포함)',style: TextStyle()),
-                    Text('금호동 3가 끌올 10분전'),
-                    Text('120,000원'),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.end,
-                      children: <Widget>[
-                        Container(
-                          margin: EdgeInsets.fromLTRB(0, 100, 0, 0),
-                        ),
-                        Icon(Icons.favorite),
-                        Text('4'),
-                      ],
-                    ),
-                  ],
-                )
-              ),
-            ],
+    return MaterialApp(
+        title: 'Bbongflix',
+        theme: ThemeData(
+            brightness: Brightness.dark,
+            primaryColor: Colors.black,
+            hintColor: Colors.white),
+        home: DefaultTabController(
+          length: 4,
+          child: Scaffold(
+            body: TabBarView(
+              physics: NeverScrollableScrollPhysics(),
+              children: <Widget>[
+                Container(child: Center(child: Text('home'),),),
+                Container(child: Center(child: Text('search'),),),
+                Container(child: Center(child: Text('save'),),),
+                Container(child: Center(child: Text('more'),),),
+              ],
+            ),
+            bottomNavigationBar: Bottom(),
           ),
-        )
-      )
-    );
+        ));
   }
 }
-//글자 위젯    Text('contents')
-//이미지 위젯   Image.asset('경로') //'assets/Kevin.png' 인식을 못한다.
-//아이콘 위젯   Icon(Icons.item)
-//박스 위젯  Container() or SizedBox()
-//여러 위젯 가로로 배치 Row( children:[]) , 세로로 배치 Column( children:[])
-//          mainAxisAlignment: MainAxisAlignment.spaceEvenly, =>display:flex 랑 비슷(정축 위주로 움직임)
-//           crossAxisAlignment: CrossAxisAlignment.center, => 세로+space 정렬
-// Scaffold() , appBar: AppBar(), body: Container(), bottomNavigationBar: BottomAppBar()
-// Row( children:[mainAxisAlignment: MainAxisAlignment.spaceEvenly])
-//레이아웃 순서 1. 원본 디자인이 필요하다 (예시 디자인) 2. 예시화면에 네모그리기 3. 바깥 네모부터 하나하나 위젯으로 4.디자인
