bccb13ad64e27cd98833a684f41614368c1b6aa6
LAGUNHO <100767288+gunho30811@users.noreply.github.com>
2023-03-08 15:11:39 +0900

    2023-03-08 15:11
    캐로셀 위젯 추가하기

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,20 +1,21 @@
 import 'package:contact_youtube/model/model_movie.dart';
+import 'package:contact_youtube/widget/carousel_slider.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
   _HomeScreenState createState() => _HomeScreenState();
 }
+
 class _HomeScreenState extends State<HomeScreen> {
-  List<Movie> movies =[
-    Movie.fromMap(
-        {
-         'title': '카지노',
-         'keyword': '도전/도박/판타지',
-         'poster': 'drama1.jpg',
-          'like': false,
-        }
-    )
+  List<Movie> movies = [
+    Movie.fromMap({
+      'title': '카지노',
+      'keyword': '도전/도박/판타지',
+      'poster': 'drama1.jpg',
+      'like': false,
+    })
   ];
+
   @override
   void initState() {
     super.initState();
@@ -22,43 +23,55 @@ class _HomeScreenState extends State<HomeScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return TopBar();
+    return ListView(
+      children: <Widget>[
+        Stack(
+          children: <Widget>[
+            CarouselImage(movies: movies),
+            TopBar(),
+          ],
+        )
+      ],
+    );
   }
 }
 
 class TopBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(padding: EdgeInsets.fromLTRB(20, 7, 20, 7),
-    child: Row(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      children:<Widget>[
-      Image.asset('images/drama1.jpg',
-        fit: BoxFit.contain,
-        height: 25,
-      ),
-      Container(
-        padding: EdgeInsets.only(right: 1),
-        child: Text(
-          'TV 프로그램',
+    return Container(
+      padding: EdgeInsets.fromLTRB(20, 7, 20, 7),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          Image.asset(
+            'images/Nlogo.png',
+            fit: BoxFit.contain,
+            height: 25,
+          ),
+          Container(
+            padding: EdgeInsets.only(right: 1),
+            child: Text(
+              'TV 프로그램',
               style: TextStyle(fontSize: 14),
-        ),
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.only(right: 1),
+            child: Text(
+              '영화',
+              style: TextStyle(fontSize: 14),
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.only(right: 1),
+            child: Text(
+              '내가 찜한 콘텐츠',
+              style: TextStyle(fontSize: 14),
+            ),
+          ),
+        ],
       ),
-      Container(
-        padding: EdgeInsets.only(right: 1),
-        child: Text(
-          '영화',
-          style: TextStyle(fontSize: 14),
-        ),
-      ),
-      Container(
-        padding: EdgeInsets.only(right: 1),
-        child: Text(
-          '내가 찜한 콘텐츠',
-          style: TextStyle(fontSize: 14),
-        ),
-      ),
-    ],),
     );
   }
 }
--- lib/widget/carousel_slider.dart
+++ lib/widget/carousel_slider.dart
@@ -1,3 +1,60 @@
+import 'dart:html';
+
+import 'package:carousel_slider/carousel_slider.dart';
+import 'package:contact_youtube/model/model_movie.dart';
 import 'package:flutter/material.dart';
 
-class CarouselImage extends
+class CarouselImage extends StatefulWidget {
+  final List<Movie> movies;
+
+  CarouselImage({required this.movies});
+
+  _CarouselImageState createState() => _CarouselImageState();
+}
+
+class _CarouselImageState extends State<CarouselImage> {
+  late List<Movie> movies;
+  late List<Widget> images;
+  late List<String> keywords;
+  late List<bool> likes;
+  int _currentPage = 0;
+  late String _currentKeyword;
+
+  @override
+  void initState() {
+    super.initState();
+    movies = widget.movies;
+    images = movies.map((m) => Image.asset('./images/' + m.poster)).toList();
+    keywords = movies.map((m) => m.keyword).toList();
+    likes = movies.map((m) => m.like).toList();
+    _currentKeyword = keywords[0];
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        children: <Widget>[
+          Container(
+            padding: EdgeInsets.all(20),
+          ),
+          CarouselSlider(
+            items: images,
+            options: CarouselOptions(onPageChanged: (index, reason) {
+              setState(
+                    () {
+                  _currentPage = index;
+                  _currentKeyword = keywords[_currentPage];
+                },
+              );
+            }),
+          ),
+          Container(
+            padding: EdgeInsets.fromLTRB(0, 10, 0, 3),
+            child: Text(_currentKeyword, style: TextStyle(fontSize: 11),),
+          )
+        ],
+      ),
+    );
+  }
+}
